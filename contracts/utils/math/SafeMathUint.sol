20e782750c5de8ff354a73fe3fff294925a9600a
syszone <syszone@hotmail.com>
2022-01-28 22:46:37 +0800

    Add files via upload

---
+++ contracts/utils/math/SafeMathUint.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.0;
+
+/**
+ * @title SafeMathUint
+ * @dev Math operations with safety TKNcks that revert on error
+ */
+library SafeMathUint {
+    function toInt256Safe(uint256 a) internal pure returns (int256) {
+        int256 b = int256(a);
+        require(b >= 0, "toInt256Safe: B LESS THAN ZERO");
+        return b;
+    }
+}
